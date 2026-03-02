85a7df02d80a77bf79f0d98496a85985ac906302
devjchapman <djchapman@uchicago.edu>
2026-03-01 18:00:54 -0600

    Update app.py

--- Code/app.py
+++ Code/app.py
@@ -1,11 +1,4 @@
-import os
 import streamlit as st
-import pydeck as pdk
-import geopandas as gpd
-from os.path import join
-import pandas as pd
-import matplotlib.pyplot as plt
-from pathlib import Path
 
 st.set_page_config(page_title="IL Data Centers", layout="wide")
 
